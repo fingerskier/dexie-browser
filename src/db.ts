20d6769521629209414640327b5431f61d6af9cc
Matt Currier <fingerskier@gmail.com>
2025-07-03 09:21:30 -0500

    Refactor code structure for improved readability and maintainability

--- src/db.ts
+++ src/db.ts
@@ -1,20 +1,37 @@
 import Dexie from 'dexie'
 import type { Table } from 'dexie'
 
+const schema = {
+  version: 1,
+  stores: {
+    dataItems: "&uuid, userId, timestamp, name, value, [userId+timestamp]"
+  }
+}
+
+
 export interface User {
   id?: number
   name: string
   email: string
 }
 
+
+export interface DataItem {
+  uuid: string
+  userId: number
+  timestamp: number
+  name: string
+  value: string
+}
+
+
 class AppDB extends Dexie {
   users!: Table<User, number>
+  dataItems!: Table<DataItem, string>
 
   constructor() {
     super('dexie-browser')
-    this.version(1).stores({
-      users: '++id, name, email'
-    })
+    this.version(1).stores(schema.stores)
   }
 }
 
