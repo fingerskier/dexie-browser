7be7dd28d866858b1e8be72bdc866e0694e881e2
Matt Currier <fingerskier@gmail.com>
2025-09-08 11:04:27 -0500

    fix user edit reverting and add realms UI (#12)

--- src/com/Realm/Edit.tsx
+++ src/com/Realm/Edit.tsx
@@ -1,3 +1,19 @@
+import { useLiveQuery } from 'dexie-react-hooks'
+import { useStateMachine } from 'ygdrassil'
+import { db } from '../../db'
+
 export default function RealmEdit() {
-  return <div>Edit Realm - TODO</div>
+  const { query } = useStateMachine()
+  const id = String(query.id)
+  const realm = useLiveQuery(() => db.realms.get(id), [id])
+
+  if (!realm) return <div>Loading...</div>
+
+  return (
+    <div>
+      <h2>{realm.name || realm.realmId}</h2>
+      <div>Realm ID: {realm.realmId}</div>
+      {realm.owner && <div>Owner: {realm.owner}</div>}
+    </div>
+  )
 }
--- src/com/Realm/List.tsx
+++ src/com/Realm/List.tsx
@@ -1,8 +1,28 @@
+import { useLiveQuery } from 'dexie-react-hooks'
+import { StateLink } from 'ygdrassil'
+import { db } from '../../db'
+
 export default function RealmList() {
+  const realms = useLiveQuery(async () => {
+    const user = db.cloud.currentUser.value
+    if (!user?.userId) return []
+    const memberships = await db.members.where('userId').equals(user.userId).toArray()
+    const realmIds = Array.from(new Set(memberships.map(m => m.realmId)))
+    if (!realmIds.length) return []
+    const res = await db.realms.bulkGet(realmIds)
+    return res.filter((r): r is NonNullable<typeof r> => !!r)
+  }, [])
+
   return (
     <div>
       <h2>Realms</h2>
-      <p>Realm management is not implemented yet.</p>
+      <ul>
+        {realms?.map(r => (
+          <li key={r.realmId}>
+            <StateLink to="realm" data={{ id: r.realmId }}>{r.name || r.realmId}</StateLink>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
