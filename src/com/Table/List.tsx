6b1cd91f151989af5dc4123f613f5dc4fe20bfd8
Matt Currier <fingerskier@gmail.com>
2025-09-10 11:30:50 -0500

    Load schema from CLI export (#15)

--- src/com/Table/List.tsx
+++ src/com/Table/List.tsx
@@ -2,7 +2,8 @@ import { StateLink } from 'ygdrassil'
 import { db } from '../../db'
 
 export default function TableList() {
-  const tables = db.tables.filter(t => !t.name.startsWith('$'))
+  const hidden = new Set(['users', 'realms', 'roles'])
+  const tables = db.tables.filter(t => !t.name.startsWith('$') && !hidden.has(t.name))
 
   return <div>
     <h2>Tables</h2>
